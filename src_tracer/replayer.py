afc683604e400b0e3e79bb42bb8dcc2fa1d8eeab
duchoang <hoangvanduccs@gmail.com>
2023-03-28 15:32:50 +0200

    add assertion point to replayer technique

--- src_tracer/replayer.py
+++ src_tracer/replayer.py
@@ -11,9 +11,11 @@ l.setLevel(logging.DEBUG)
 
 class Replayer(ExplorationTechnique):
 
-    def __init__(self, trace: Trace, functions=None):
+    def __init__(self, trace: Trace, functions=None, assert_points: [(int, str)]=None):
         super().__init__()
         self.iterator = trace.__iter__()
+        self.assert_points = assert_points
+
         try:
             self.choice = next(self.iterator)
         except StopIteration:
@@ -26,6 +28,15 @@ class Replayer(ExplorationTechnique):
         self.else_addr = self._addr("_retrace_else")
         self.wrote_int_addr = self._addr("_retrace_wrote_int")
         self.int_addr = self._addr("_retrace_int")
+        # calculate mapped_base to add to addr
+        mapped_base = simgr.one_active.project.loader.main_object.mapped_base
+        self.assertion_addr = []
+        if self.assert_points is None:
+            return
+        for assert_point in self.assert_points:
+            addr, assertion = assert_point
+            self.assertion_addr.append(addr + mapped_base)
+
 
     def _addr(self, sym_name):
         try:
@@ -42,12 +53,13 @@ class Replayer(ExplorationTechnique):
             self.choice = None
 
     def step(self, simgr, stash='active', **kwargs):
+        read_trace = True
         elem, bs = self.choice
         if elem == 'T':
-            find = self.if_addr
+            find = [self.if_addr]
             avoid = [self.else_addr, self.wrote_int_addr]
         elif elem == 'N':
-            find = self.else_addr
+            find = [self.else_addr]
             avoid = [self.if_addr, self.wrote_int_addr]
         elif self.functions and 'F' in elem:
             if bs == b'':
@@ -57,10 +69,10 @@ class Replayer(ExplorationTechnique):
             func_name = self.functions["hex_list"][func_num]["name"]
             if func_name == "main":
                 func_name = "main_original"
-            find = self._addr(func_name)
+            find = [self._addr(func_name)]
             avoid = [self.else_addr, self.if_addr, self.wrote_int_addr]
         elif elem == 'D':
-            find = self.wrote_int_addr
+            find = [self.wrote_int_addr]
             avoid = [self.else_addr, self.if_addr]
         else:
             raise ValueError(f'Trace contains unsupported element "{elem}"')
@@ -68,12 +80,26 @@ class Replayer(ExplorationTechnique):
         # step to make sure the current state is not in one of the avoid state
         if self.trace_ind != 0:
             simgr.step()
+
+        # add all the assertion point
+        find.extend(self.assertion_addr)
         simgr.explore(find=find, avoid=avoid, avoid_priority=True)
 
         if len(simgr.found) != 1:
             l.error("Found %i canditates in simgr %s", len(simgr.found), simgr)
 
-        if elem == 'D':
+        if len(simgr.found) == 1 and simgr.found[0].addr in self.assertion_addr:
+            # TODO: add assertion here
+            # ....
+
+            # remove the assertion addr from list
+            self.assertion_addr.remove(simgr.found[0].addr)
+
+            # set to not read the next trace element
+            read_trace = False
+
+
+        if read_trace and elem == 'D':
             # add the constrain for the int
             trace_int = int.from_bytes(bs, "little")
             state = simgr.found[0]
@@ -84,14 +110,18 @@ class Replayer(ExplorationTechnique):
         simgr.drop()
 
         self.trace_ind = 1
-        if bs == b'':
+
+        if not read_trace:
+            l.debug("insert assertion in %s", simgr.found[0].__str__())
+        elif bs == b'':
             l.debug("trace with %s, arrive at %s", elem, simgr.found[0].__str__())
         else:
             l.debug("trace with %s, arrive at %s", elem + str(int.from_bytes(bs, "little")), simgr.found[0].__str__())
 
         simgr.move(from_stash='found', to_stash='active')
 
-        self._next_choice()
+        if read_trace:
+            self._next_choice()
 
         return simgr
 
