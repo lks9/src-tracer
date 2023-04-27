bf1ec5e507fb9c63d218a8633193f66ea201d16f
duchoang <hoangvanduccs@gmail.com>
2023-04-27 12:44:46 +0200

    add util class

---
+++ src_tracer/util.py
@@ -0,0 +1,14 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+class Util:
+
+    @classmethod
+    def get_name(cls, cursor, func_num):
+        cursor.execute("SELECT name FROM automatic_lookup WHERE num=?", (func_num,))
+        name = cursor.fetchone()
+        if name is None:
+            log.error("there is no name (entry) for the func_num (%d)", func_num)
+            return None
+        return name[0]
