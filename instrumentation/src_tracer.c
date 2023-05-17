1f0fbf9676b4c24818978e69fb37c17e9ad4dc0e
Lukas Grätz <lukas.graetz@tu-darmstadt.de>
2023-05-17 09:26:08 +0200

    remove unused syscall stuff

--- instrumentation/src_tracer.c
+++ instrumentation/src_tracer.c
@@ -21,51 +21,6 @@ int _trace_fd = 0;
 unsigned char _trace_if_byte = _TRACE_SET_IE;
 int _trace_if_count = 0;
 
-#ifndef _TRACE_USE_POSIX_WRITE
-// taken from musl (arch/x86_64/syscall_arch.h)
-
-static __inline long __syscall1(long n, long a1)
-{
-	unsigned long ret;
-	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1) : "rcx", "r11", "memory");
-	return ret;
-}
-
-static __inline long __syscall2(long n, long a1, long a2)
-{
-	unsigned long ret;
-	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2)
-						  : "rcx", "r11", "memory");
-	return ret;
-}
-
-static __inline long __syscall3(long n, long a1, long a2, long a3)
-{
-	unsigned long ret;
-	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
-						  "d"(a3) : "rcx", "r11", "memory");
-	return ret;
-}
-
-static __inline long __syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
-{
-	unsigned long ret;
-	register long r10 __asm__("r10") = a4;
-	register long r8 __asm__("r8") = a5;
-	register long r9 __asm__("r9") = a6;
-	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
-						  "d"(a3), "r"(r10), "r"(r8), "r"(r9) : "rcx", "r11", "memory");
-	return ret;
-}
-
-#define SYS_write				1
-#define SYS_mmap				9
-#define SYS_mprotect			10
-#define SYS_munmap				11
-#define SYS_ftruncate			77
-// end musl code
-#endif
-
 static char dummy;
 char *_trace_ptr = &dummy;
 bool _trace_ptr_count = 0;
