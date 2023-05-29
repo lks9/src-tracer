6e9f1d459b682cde1a709d13b8057d30a0debb18
Lukas Grätz <lukas.graetz@tu-darmstadt.de>
2023-05-29 15:46:46 +0200

    make if byte register work

--- instrumentation/src_tracer.h
+++ instrumentation/src_tracer.h
@@ -37,7 +37,7 @@ struct _trace_ctx {
     bool active;
 };
 
-#define _TRACE_IF_BYTE_INIT       ((1 << 14))
+#define _TRACE_IF_BYTE_INIT       0x8008
 register short int _trace_if_byte __asm__ ("r12");
 
 extern struct _trace_ctx _trace;
@@ -88,22 +88,16 @@ extern int _trace_after_fork(int pid);
 #define _TRACE_PUT_TEXT     _TRACE_PUT
 
 #define _TRACE_IE(if_true) ;{ \
-        _trace_if_byte >>= 1; \
-        _trace_if_byte |= ((short)(if_true)) << 6; \
-        if (_trace_if_byte & (1 << 7)) { \
-            _TRACE_PUT((char)(_trace_if_byte)); \
+        _trace_if_byte |= ((short)(if_true)) << (_trace_if_byte & 0xf); \
+        _trace_if_byte += 1; \
+        if ((_trace_if_byte & 0xf) == 0xf) { \
+            _TRACE_PUT((char)(_trace_if_byte >> 8)); \
             _trace_if_byte = _TRACE_IF_BYTE_INIT; \
         } \
     }
 
-#define _SHIFT_TO_NUM(shift) \
-    ( ((shift & 0b0010101010000000) ? 1 : 0) \
-    | ((shift & 0b0001100110000000) ? 2 : 0) \
-    | ((shift & 0b0000011110000000) ? 4 : 0) \
-    )
-
 #define _IF_COUNT \
-    _SHIFT_TO_NUM(_trace_if_byte)
+    (_trace_if_byte & 0x7)
 
 #define _TRACE_FUNC(num) ;{ \
     if ((num) == 0) { \
